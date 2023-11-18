0d7f39898f35ef770fe9de0d671011361fc29e87
Mudit <muditp22@gmail.com>
2023-11-18 11:29:43 +0530

    CheckHealthWinningStrategy updated

--- src/main/java/assignment/magicalArena/winStrategy/CheckHeathWinningStrategy.java
+++ src/main/java/assignment/magicalArena/winStrategy/CheckHeathWinningStrategy.java
@@ -1,10 +1,11 @@
 package assignment.magicalArena.winStrategy;
 
 import assignment.magicalArena.model.Move;
+import assignment.magicalArena.model.Player;
 
 public class CheckHeathWinningStrategy implements GameWinningStrategy{
     @Override
-    public boolean checkWinner(Move move) {
-        return false;
+    public boolean checkWinner(Player Attacker) {
+        if(Attacker.getHealth() <= 0)
     }
 }
